f4162e239c76c9281d3fc00d5520e4c47dfe7e29
Cv Vikash <cvvkshcv@gmail.com>
2017-11-21 01:11:09 +0530

    Routes added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,50 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import { RouterModule, Routes } from '@angular/router';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule ,MatButtonModule, MatCheckboxModule ,MatSelectModule,MatOption } from '@angular/material';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+// import { AddDoctorComponent } from './add-doctor/add-doctor.component';
+import { SearchDoctorComponent } from './search-doctor/search-doctor.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ViewDoctorComponent } from './view-doctor/view-doctor.component';
+
+
+const appRoutes: Routes = [
+  {path : 'home' , component : HomeComponent},
+  {path : 'login' , component : LoginComponent},
+  {path : 'register' , component : RegisterComponent},
+  {path : 'search-doctor' , component : SearchDoctorComponent},
+  {path : 'view-doctor/:id' , component : ViewDoctorComponent},
+  {path : '**' , component : HomeComponent}
+];
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    // AddDoctorComponent,
+    SearchDoctorComponent,
+    LoginComponent,
+    RegisterComponent,
+    ViewDoctorComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatButtonModule,
-    MatCheckboxModule
+    RouterModule.forRoot(appRoutes),
+    MatButtonModule,MatCheckboxModule,MatIconModule,MatMenuModule,MatFormFieldModule,MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
